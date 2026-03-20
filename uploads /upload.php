291400a80abb5eb7619de2f41bdff10af1decc1c
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-20 00:00:59 -0400

    move file from temporary location to uplaod folder

--- uploads /upload.php
+++ uploads /upload.php
@@ -17,11 +17,33 @@
             $tempName=$_FILES['profileImage']['tmp_name'];
             $fileSize=$_FILES['profileImage']['size'];
 
+            $uploadFolder="uploads/";
+            $targetFile=$uploadFolder.basename($fileName);
 
-            
+            $imageFileType=strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+            $allowedTypes=["jpg","jpeg","png","gif","webp"];
+
+            if(in_array($imageFileType, $allowedTypes)){
+                echo"<p> image uploaded successfully!</p>";
+                echo"<p>file name:" .htmlspecialchars($fileName)."</p>";
+                echo"<img src='$targetFile' alt='Uploaded Image' width='300'>";
+            }else{
+                echo"<p>sorry, there is the problem.</p>";
+            }
+            else{
+                echo"<p> only jpg, jpeg, png, Gif, and WEBP files are allowed.</p>;";
+            }
+            else{
+                echo"<p> no file uploaded or there is an error.</p>";
+            }
+            else{
+                echo"<p> please upload an image using form.</p>";
+            }
+            }
+
         }
 
-    }
+    
     ?>
 
     </body>
