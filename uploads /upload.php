9fe1b95e228060bdcdf84992b9a926642e2ca8bb
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-19 23:07:36 -0400

    file name, tmp, size

--- uploads /upload.php
+++ uploads /upload.php
@@ -8,8 +8,21 @@
     <body>
 
     <h1>upload result</h1>
-    
 
+    <?php
+    if(isset($_POST['submit'])){
+        if(isset($_FILES['profileImage']) && $_FILES['profileImage']['error']===0){
+
+            $fileName=$_FILES['profileImage']['name'];
+            $tempName=$_FILES['profileImage']['tmp_name'];
+            $fileSize=$_FILES['profileImage']['size'];
+
+
+            
+        }
+
+    }
+    ?>
 
     </body>
 </html>
