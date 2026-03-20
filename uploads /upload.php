5ebf5d24929ed9c1f78f0c0caa8117f74cb370a4
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-19 21:45:07 -0400

    upload file/html

---
+++ uploads /upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+        <title>upload result</title>
+    </head>
+    <body>
+
+    <h1>upload result</h1>
+    
+
+
+    </body>
+</html>
