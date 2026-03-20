6d5f40961f1887c2fa5f20978311f277b6bbb60b
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-19 21:08:39 -0400

    starting html

---
+++ uploads /index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, intial-scale=1.0">
+<title>Lab-five-upload profile picture</title>
+
+
+</html>
